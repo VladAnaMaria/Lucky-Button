cdce81a523855a39bc39219d5e1152fff6469bd3
VladAnaMaria <123776957+VladAnaMaria@users.noreply.github.com>
2023-01-30 23:38:03 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+    var luckyButton = 0
+function getLuckyButton() {
+    luckyButton = Math.floor(Math.random() * 3 + 1);
+}
+function getMessage(clicked_id) {
+    if (clicked_id == luckyButton) {
+        document.getElementById("message").innerHTML="Congratulations! You won a 50% discount code!";
+        document.getElementById("message").style="color:#090;padding:10px;border:1px solid #bbb;";
+        
+
+    } else {
+        document.getElementById("message").innerHTML="Sorry! But we offer you a 5% discount code!";
+        document.getElementById("message").style="padding:10px;border:1px solid #bbb;";
+    }
+    document.getElementById("1").disabled = "true";
+    document.getElementById("2").disabled = "true";
+    document.getElementById("3").disabled = "true";
+}
